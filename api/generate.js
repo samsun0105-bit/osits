93186b9de9f130fb7a3bed1c21098b6bf4bfbe4e
職業安全管理師 <samsun0105@gmail.com>
2026-05-18 09:16:31 +0800

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -15,7 +15,7 @@ export default async function handler(req, res) {
     }
 
     const apiUrl =
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
+      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${AIzaSyC-xGHGMAUxBXor4cZfmaLh8sgeI4EnlQg}`;
 
     const response = await fetch(apiUrl, {
       method: "POST",
